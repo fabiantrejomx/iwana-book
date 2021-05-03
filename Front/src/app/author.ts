3b88f8217b35ce1f66f6df710a930b53d49a0e56
Kats18 <cneto@live.com.mx>
2021-05-03 02:26:05 -0500

    interfaces y book.html

---
+++ Front/src/app/author.ts
@@ -0,0 +1,7 @@
+export interface Author {
+    id: number;
+    name: string;
+    firstName: string;
+    secondName: string;
+    birthday: string;
+}
---
+++ Front/src/app/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+    title: string;
+    pages: number;
+    isbn: string;
+    price: number;
+    summary: string;
+    editorial: string;
+    releaseDate: Date;
+    categories: string;
+    authors: string;
+}
